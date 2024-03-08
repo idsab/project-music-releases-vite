8ce8610dea6ee65929a9c00c4c431c10bba33f6b
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-03-08 09:58:11 +0100

    Added clickable urls for artists

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -2,7 +2,6 @@ import "./Albums.css";
 import data from "../data/albums.json";
 import { AlbumName } from "./children/AlbumName";
 import { ArtistName } from "./children/ArtistName";
-
 import { AlbumCover } from "./children/AlbumCover";
 
 export const Albums = () => {
@@ -23,8 +22,7 @@ export const Albums = () => {
       <div className="album-items" key={id}>
         <AlbumCover images={images[1].url} />
         <AlbumName name={name} external_urls={external_urls.spotify} />
-
-        <ArtistName name={artists[0].name} />
+        <ArtistName name={artists[0].name} external_urls={artists[0].external_urls.spotify} />
       </div>
     )
   );
